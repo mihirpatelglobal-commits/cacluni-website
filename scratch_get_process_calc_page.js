904c52295c7c9fa4f742aa24aed252309d711e11
Mihir Patel <mihirpatelglobal@gmail.com>
2026-07-10 17:03:29 -0600

    chore: remove scratch/debug files, add .gitignore for future scratch files

--- scratch_get_process_calc_page.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('update_layout.js', 'utf8');
-const lines = content.split('\n');
-for (let i = 401; i < 600; i++) {
-  console.log(`${i + 1}: ${lines[i]}`);
-}
